15794c92dd140d36cbd824f3735866e17d0dfdbb
mofon <immofon@163.com>
2021-01-07 09:19:18 +0800

    update

--- src/mFEM.jl
+++ src/mFEM.jl
@@ -1,5 +1,5 @@
 module mFEM
-
+using Printf
 # N:int  
 # Nm:int 
 # P:mat 
@@ -86,15 +86,107 @@ function mesh(a,b,N)::mFEM.Mesh
   return mFEM.Mesh(P,size(P,1),T,size(T,1),gauss_quad)
 end
 
-function linear_elem(mesh::mFEM.Mesh)::mFEM.Basis
-  Pb = copy(mesh.P)
-  Tb = copy(mesh.T)
-  Nlb = 2
 
+function Dϕ_reference_linear(k::Int, xt::Tuple, α::Int)
+  @assert α ∈ [1,2]
+  @assert k ∈ [0,1]
+
+  local x = xt[1]
+
+  if α == 1
+    if k == 0
+      return 1 - x
+    elseif k == 1
+      return  -1
+    end
+  elseif  α == 2
+    if k == 0
+      return x
+    elseif k == 1
+      return 1
+    end
+  end
+end
+
+function Dϕ_reference_quadratic(k::Int, xt::Tuple, α::Int)
+  @assert α ∈ [1,2,3]
+  @assert k ∈ [0,1,2]
+
+  local x = xt[1]
+
+  if α == 1
+    if k == 0
+      return 2x^2 - 3x + 1
+    elseif k == 1
+      return 4x - 3
+    elseif k ==2
+      return 4
+    end
+  elseif  α == 2
+    if k == 0
+      return 2x^2 - x
+    elseif k == 1
+      return 4x - 1
+    elseif k ==2
+      return 4
+    end
+  elseif  α == 3
+    if k == 0
+      return -4x^2 + 4x
+    elseif k == 1
+      return -8x + 4
+    elseif k ==2
+      return -8
+    end
+  end
+end
+
+function Dϕ_reference_cubic(k::Int, xt::Tuple, α::Int)
+  @assert α ∈ 1:4
+  @assert k ∈ 0:2
+
+  local x = xt[1]
+
+  if α == 1
+    if k == 0
+      return -4.5x^3 + 9x^2 -5.5x + 1
+    elseif k == 1
+      return -3*4.5x^2 + 2*9x -5.5
+    elseif k == 2
+      return -2*3*4.5x + 2*9
+    end
+  elseif α == 2
+    if k == 0
+      return 4.5x^3 - 4.5x^2 + x
+    elseif k == 1
+      return 3*4.5x^2 - 2*4.5x + 1
+    elseif k == 2
+      return 2*3*4.5x - 2*4.5
+    end
+  elseif α == 3
+    if k == 0
+      return 13.5x^3 - 22.5x^2 + 9x
+    elseif k == 1
+      return 3*13.5x^2 - 2*22.5x + 9
+    elseif k == 2
+      return 2*3*13.5x - 2*22.5
+    end
+  elseif α == 4
+    if k == 0
+      return -13.5x^3 + 18x^2 - 4.5x
+    elseif k == 1
+      return -3*13.5x^2 + 2*18x - 4.5
+    elseif k == 2
+      return -2*3*13.5x + 2*18
+    end
+  end
+end
+
+function reference2local(Dϕ_reference::Function;Pb::Array,Tb::Array,Nlb::Int)::Function
   # kth deriavtive of basis function at x
   function Dϕ(k::Int, xt::Tuple, n::Int, α::Int)
-    @assert α ∈ [1,2]
-    @assert k ∈ [0,1]
+    @assert α ∈ 1:Nlb
+    @assert k ∈ [0,1,2]
     @assert length(xt) == 1
 
     x = xt[1]
@@ -102,26 +194,78 @@ function linear_elem(mesh::mFEM.Mesh)::mFEM.Basis
     xn1 = Pb[Tb[n,1],1]
     xn2 = Pb[Tb[n,2],1]
 
-    if α == 1
-      if k == 0
-        return (xn2 - x) / (xn2 - xn1);
-      elseif k == 1
-        return  - 1 / (xn2 - xn1);
-      else
-        return 0
-      end
-    elseif α == 2
-      if k == 0
-        return (x - xn1) / (xn2 - xn1);
-      elseif k == 1
-        return 1 / (xn2 - xn1);
-      else
-        return 0
-      end
+    x_hat = (x - xn1) / (xn2 - xn1)
+    xt_hat = (x_hat,)
+
+    if k == 0
+      return Dϕ_reference(k,xt_hat,α)
+    elseif k == 1
+      return Dϕ_reference(k,xt_hat,α) / (xn2 - xn1)
+    elseif k == 2
+      @error "TODO"
     end
   end
 
-  return mFEM.Basis(size(Pb,1),Pb,Tb,Nlb,Dϕ)
+  return Dϕ
+end
+
+function linear_elem(mesh::mFEM.Mesh)::mFEM.Basis
+  Pb = copy(mesh.P)
+  Tb = copy(mesh.T)
+  Nlb = 2
+
+  return mFEM.Basis(size(Pb,1),Pb,Tb,Nlb,
+    reference2local(Dϕ_reference_linear;Pb=Pb,Tb=Tb,Nlb=Nlb))
+end
+
+function quadatic_elem(mesh::mFEM.Mesh)::mFEM.Basis
+  Pb = zeros(Float64,size(mesh.P,1) * 2 - 1,1)
+  Tb = zeros(Int,size(mesh.T,1),3)
+  Nlb = 3
+
+  for i in 1:size(mesh.P,1)
+    Pb[2i - 1] = mesh.P[i]
+  end
+
+  for i in 1:(size(mesh.P,1) - 1)
+    Pb[2i] = (Pb[2i-1] + Pb[2i+1]) / 2
+  end
+  
+  for i in 1:size(mesh.T,1)
+    Tb[i,1] = 2 * mesh.T[i,1] - 1
+    Tb[i,2] = 2 * mesh.T[i,2] - 1
+    Tb[i,3] = (Tb[i,1] + Tb[i,2]) / 2
+  end
+
+  return mFEM.Basis(size(Pb,1),Pb,Tb,Nlb,
+    reference2local(Dϕ_reference_quadratic;Pb=Pb,Tb=Tb,Nlb=Nlb)
+  )
+end
+
+function cubic_elem(mesh::mFEM.Mesh)::mFEM.Basis
+  Pb = zeros(Float64,size(mesh.P,1) * 3 - 2,1)
+  Tb = zeros(Int,size(mesh.T,1),4)
+  Nlb = 4
+
+  for i in 1:size(mesh.P,1)
+    Pb[3i - 2] = mesh.P[i]
+  end
+  for i in 1:(size(mesh.P,1) - 1)
+    Pb[3i - 1] = (2/3)* Pb[3i-2] + (1/3) * Pb[3i + 1]
+    Pb[3i] = (1/3)* Pb[3i-2] + (2/3) * Pb[3i + 1]
+  end
+
+  for i in 1:size(mesh.T,1)
+    Tb[i,1] = 3 * mesh.T[i,1] - 2
+    Tb[i,2] = 3 * mesh.T[i,2] - 2
+    Tb[i,3] = Tb[i,1] + 1
+    Tb[i,4] = Tb[i,1] + 2
+  end
+
+
+  return mFEM.Basis(size(Pb,1),Pb,Tb,Nlb,
+    reference2local(Dϕ_reference_cubic;Pb=Pb,Tb=Tb,Nlb=Nlb)
+  )
 end
 
 # - D(cDu) = f in Ω, f=g on ∂Ω
@@ -179,22 +323,23 @@ function set_boundary_cond!(trial::mFEM.Basis,boundarynodes::AbstractArray,
   end
 end
 
-function Possion(mesh::mFEM.Mesh,c::Function,f::Function,g::Function)
-  trial = linear_elem(mesh)
-  test = linear_elem(mesh)
-
+function Possion(mesh::mFEM.Mesh,c::Function,f::Function,g::Function;
+    trial::mFEM.Basis = linear_elem(mesh),test::mFEM.Basis = linear_elem(mesh))
   # generate boundary nodes
   boundarynodes = [1 1;trial.Nb 1]
 
   A = stiffness_mat(mesh,trial,test,c)
   b = load_vec(mesh,test,f)
 
+
   set_boundary_cond!(trial,boundarynodes,A,b,g)
   x = A \ b
   return x
 end
 
-function test()
+
+
+function test(n,;finite_elem::Function=linear_elem)
   u(x) = x * cos(x)
   c(x) = exp(x)
   f(x) = - exp(x) *(cos(x) - 2sin(x) - x*cos(x) - x*sin(x))
@@ -204,11 +349,41 @@ function test()
     return (x::Tuple) -> f(x[1])
   end
 
-  m = mesh(0,1,128)
+  m = mesh(0,1,n)
+  trial = finite_elem(m)
+  test= finite_elem(m)
 
-  return map(u,m.P) - Possion(m,tuplfy1d(c),tuplfy1d(f),g)
+  U = Possion(m,tuplfy1d(c),tuplfy1d(f),g;trial=trial,test=test)
+  return maximum(@.abs(U-map(u,trial.Pb)))
 end
 
+test_quadatic(n) = test(n,finite_elem=quadatic_elem)
+test_linear(n) = test(n,finite_elem=linear_elem)
+test_cubic(n) = test(n,finite_elem=cubic_elem)
+
 end # Dim1
+
+# for test
+
+# test()::Float64, error of method
+function convergence_order(test::Function)
+  N = [2^i for i=2:7]
+  Error = zeros(Float64,size(N,1))
+  Order = zeros(Float64,size(N,1))
+
+  for i in 1:size(N,1)
+    n = N[i]
+    Error[i] = test(n)
+  end
+
+  for i in size(Error,1):-1:2
+    Order[i] = Error[i-1] / Error[i]
+  end
+
+  println("   n\terror\t\torder")
+  for i in 1:size(N,1)
+    @printf "%4d %.10e\t%.2f\tO(h^%.6f)\n" N[i] Error[i] Order[i] log(2,Order[i])
+  end
+end
 
 end # mFEM
